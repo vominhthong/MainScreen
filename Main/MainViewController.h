7c45cf735c54c5b5e8a2577285f7f3e2989c6bca
vominhthong <santalucia1992@gmail.com>
2013-06-18 01:03:42 +0700

    -v1.0

---
+++ Main/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Main
+//
+//  Created by MC372 on 6/18/13.
+//  Copyright (c) 2013 MC372. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
